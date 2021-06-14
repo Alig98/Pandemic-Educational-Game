dd3c7042c00e3c8cde0462d66965a86d57ec1f24
Alig98 <aligolgeli98@gmail.com>
2021-06-14 22:03:19 +0300

    Mask level almost done
    Almost finished the mask level all mechanics are done.

---
+++ Pandemic Educational Game/Assets/Scripts/HeadController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    private Vector3 slide;
+    public float slidePower = 5;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        slide = new Vector3(Input.GetAxis("Mouse X"),0,0);
+    }
+    private void OnMouseDrag()
+    {
+        transform.position += slide*slidePower*Time.deltaTime;
+    }
+}
---
+++ Pandemic Educational Game/Assets/Scripts/Mask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    public float RotationSpeed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnMouseDrag()
+    {
+        transform.Rotate((Input.GetAxis("Mouse Y") * RotationSpeed), 0, 0, Space.World);
+    }
+}
