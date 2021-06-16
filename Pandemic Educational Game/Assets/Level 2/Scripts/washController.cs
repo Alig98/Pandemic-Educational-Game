86d0d51ab992a93f1ff3f0759cf1fe443faf418f
Alig98 <aligolgeli98@gmail.com>
2021-06-16 04:22:26 +0300

    Level 2 added
    All level 2 mechancis modelled, animated and added

---
+++ Pandemic Educational Game/Assets/Level 2/Scripts/washController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class washController : MonoBehaviour
+{
+    private Animator myAnim;
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myAnim.SetBool("isWashing", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            myAnim.SetBool("isWashing", false);
+        }
+    }
+}
