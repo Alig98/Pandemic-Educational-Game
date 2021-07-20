fb135b8eff32f6d680ceb739ec0d2e2c88c95cee
Alig98 <aligolgeli98@gmail.com>
2021-07-21 01:59:12 +0300

    Level 4
    Level 4 almost done

---
+++ Pandemic Educational Game/Assets/Level 4/Script/HeatScan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HeatScan : MonoBehaviour
+{
+    float heat;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        heat = Random.Range((float)36,(float)40);
+        other.gameObject.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = heat.ToString("0.#");
+    }
+}
