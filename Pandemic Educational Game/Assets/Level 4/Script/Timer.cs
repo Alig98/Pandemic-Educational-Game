63b31eb3d9045892e688ea846e582599219ce11b
Alig98 <aligolgeli98@gmail.com>
2021-07-21 19:50:11 +0300

    Level 4 and level 1 update
    Made timer with slider. New icons added to level 1 and level 4.

---
+++ Pandemic Educational Game/Assets/Level 4/Script/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timer;
+    void Start()
+    {
+        timer.maxValue = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.value += Time.deltaTime;
+    }
+}
